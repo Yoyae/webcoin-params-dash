66d697be70e08786f9932bae02ab47808cbe5b5d
Yoyae <gingin.fr@gmail.com>
2018-02-19 15:29:55 +0100

    Monoecify

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,6 +1,6 @@
 // blockchain definition
 
-var u = require('dash-util')
+var u = require('monoeci-util')
 var BN = require('bn.js')
 var reverse = require('buffer-reverse')
 var repeat = require('repeat-string')
